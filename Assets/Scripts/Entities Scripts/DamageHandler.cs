82b5b8ab0cac38b33f0e801e7712bf7b5ea2b34b
Szymon Świędrych <szymek_sw@outlook.com>
2021-08-17 19:15:53 +0200

    Add Entity\DamageHandler

---
+++ Assets/Scripts/Entities Scripts/DamageHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Entities_Scripts
+{
+    public class DamageHandler : MonoBehaviour
+    {
+        public UnityEvent onTakeDamage;
+        public int TakenDamage { get; private set; } 
+        
+        public void TakeDamage(int damage)
+        {
+            TakenDamage += damage;
+            onTakeDamage?.Invoke();
+        }
+    }
+}
--- Assets/Tank/Scripts/Shooting/Explosion/Explosion.cs
+++ Assets/Tank/Scripts/Shooting/Explosion/Explosion.cs
@@ -1,3 +1,4 @@
+using Entities_Scripts;
 using Pool;
 using UnityEngine;
 
@@ -5,29 +6,18 @@ namespace Tank.Scripts.Shooting.Explosion
 {
 	public class Explosion : MonoBehaviour
 	{
-		private ParticleEffectHandler particleEffectHandler;
 		[SerializeField] private float timeToAnimationOver = 2f;
+		[SerializeField] private float radius = 3f;
+		[SerializeField] private float force = 100f;
+		[SerializeField] private int damage = 100;
+
 
 		private Timer destroyTimer;
+		private ParticleEffectHandler particleEffectHandler;
 
 		private void Awake()
 		{
 			particleEffectHandler = GetComponent<ParticleEffectHandler>();
-
-		}
-
-		private void OnEnable()
-		{
-			destroyTimer = new Timer(timeToAnimationOver);
-			destroyTimer.ResetTimer();
-			destroyTimer.OnTimerEnd += ReturnToPool;
-
-			particleEffectHandler.Play();
-		}
-
-		private void OnDisable()
-		{
-			destroyTimer.OnTimerEnd -= ReturnToPool;
 		}
 
 		private void Update()
@@ -35,10 +25,69 @@ namespace Tank.Scripts.Shooting.Explosion
 			destroyTimer.Update();
 		}
 
+		private void OnEnable()
+		{
+			PrepareTimer();
+			Explode();
+			destroyTimer.OnTimerEnd += ReturnToPool;
+		}
+
+		private void OnDisable()
+		{
+			destroyTimer.OnTimerEnd -= ReturnToPool;
+		}
+
+		private void PrepareTimer()
+		{
+			destroyTimer = new Timer(timeToAnimationOver);
+			destroyTimer.ResetTimer();
+		}
+
 		private void ReturnToPool()
 		{
 			GetComponent<PoolMember>().ReturnToPool();
 		}
+
+		private void Explode()
+		{
+			var hitColliders = GetNearbyColliders();
+			ApplyPhysics(hitColliders);
+			ApplyDamage(hitColliders);
+			particleEffectHandler.Play();
+		}
+
+		private void ApplyPhysics(Collider[] nearbyColliders)
+		{
+			foreach (var hitCollider in nearbyColliders) ApplyExplosionForceToHitRigidbody(hitCollider);
+		}
+
+		private Collider[] GetNearbyColliders()
+		{
+			var hitColliders = Physics.OverlapSphere(transform.position, radius);
+			return hitColliders;
+		}
+
+		private void ApplyExplosionForceToHitRigidbody(Collider hitCollider)
+		{
+			var hitRigidbody = hitCollider.GetComponent<Rigidbody>();
+			if (hitRigidbody == null) return;
+			
+			hitRigidbody.AddExplosionForce(force, transform.position, radius);
+		}
 		
+		private void ApplyDamage(Collider[] nearbyColliders)
+		{
+			foreach (var hitCollider in nearbyColliders) ApplyExplosionDamageToHitRigidbody(hitCollider);
+		}
+
+		private void ApplyExplosionDamageToHitRigidbody(Collider hitCollider)
+		{
+			var hitDamageHandler = hitCollider.GetComponent<DamageHandler>();
+			if (hitDamageHandler == null) return;
+			
+			//TODO Distance to damage
+			
+			hitDamageHandler.TakeDamage(damage);
+		}
 	}
 }
